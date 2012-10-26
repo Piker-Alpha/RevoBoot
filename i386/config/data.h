2aced469506a483bc6a392f6709bae655394a390
Piker-Alpha <PikerAlpha@yahoo.com>
2012-10-26 11:14:00 +0300

    Update for RevoBoot v1.5.10/v1.5.20

--- i386/config/data.h
+++ i386/config/data.h
@@ -1,19 +1,31 @@
 /*
  * Copyright (c) 2009 Master Chief.
- * Refactored by DHP in 2010-2011.
+ *
+ * This file, included by acpi.c, efi.c and smbios.c includes data files
+ * for the target build model (based on MODEL=MacModelNN) or default.h
+ *
+ * Updates:
+ *
+ *			- Refactored by DHP in 2010-2011.
+ *			- Automatic model data selection (PikerAlpha, October 2012).
+ *
  */
 
 
-#if PATCH_ACPI_TABLE_DATA && INCLUDE_ACPI_DATA
+#if (PATCH_ACPI_TABLE_DATA && INCLUDE_ACPI_DATA)
 	#undef INCLUDE_ACPI_DATA
-	#include "acpi/data.h"
 
-#elif INJECT_EFI_DEVICE_PROPERTIES && INCLUDE_EFI_DATA
+	#include STRING(ACPI_DATA_FILE)
+#endif
+
+#if (INJECT_EFI_DEVICE_PROPERTIES && INCLUDE_EFI_DATA)
 	#undef INCLUDE_EFI_DATA
-	#include "efi/data.h"
 
-#elif USE_STATIC_SMBIOS_DATA && INCLUDE_SMBIOS_DATA
+	#include STRING(EFI_DATA_FILE)
+#endif
+
+#if (USE_STATIC_SMBIOS_DATA && INCLUDE_SMBIOS_DATA)
 	#undef INCLUDE_SMBIOS_DATA
-	#include "smbios/data.h"
 
+	#include STRING(SMBIOS_DATA_FILE)
 #endif
