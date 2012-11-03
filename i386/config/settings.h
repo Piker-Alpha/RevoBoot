0fa8bfae4bf33d5e3cc1c179bdcf23c361038803
Pike <PikerAlpha@yahoo.com>
2012-11-03 19:33:03 +0100

    Patch for RevoBoot v1.5.34

--- i386/config/settings.h
+++ i386/config/settings.h
@@ -3,10 +3,17 @@
  *
  * Updates:
  *
- *			- Read settings file based on given model identifier (PikerAlpha, October 2012).
+ *  		- Read settings file based on given model identifier (PikerAlpha, October 2012).
+ *			- COMMA_STRIPPED_MODEL_ID added (PikerAlpha, November 2012).
  */
 
-#define TO_STRING_DO(a)		#a
-#define STRING(a)			TO_STRING_DO(a)
+#define TO_STRING_DO(a)						#a
+#define STRING(a)							TO_STRING_DO(a)
 
 #include STRING(SETTINGS_FILE)
+
+/*
+ * gPlatform.ModelID is a char * initialized by a call to strdup(SMB_PRODUCT_NAME) in
+ * platform.c and we use strdup once more here so that gPlatform.ModelID is untouched.
+ */
+#define COMMA_STRIPPED_MODEL_ID	removeChar(strdup(gPlatform.ModelID), ',')
