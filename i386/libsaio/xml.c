8fd3a91d194f1e26a7b3ff854fe70bc688bc7875
Piker-Alpha <PikerAlpha@yahoo.com>
2013-03-19 17:22:43 +0100

    Whitespace changes only

--- i386/libsaio/xml.c
+++ i386/libsaio/xml.c
@@ -2,15 +2,15 @@
  * Copyright (c) 2003 Apple Computer, Inc. All rights reserved.
  *
  * @APPLE_LICENSE_HEADER_START@
- * 
+ *
  * Portions Copyright (c) 2003 Apple Computer, Inc.  All Rights
- * Reserved.  
+ * Reserved.
  * The contents of this file constitute Original Code as defined in and
  * are subject to the Apple Public Source License Version 2.0 (the
  * "License").  You may not use this file except in compliance with the
  * License.  Please obtain a copy of the License at
  * http://www.apple.com/publicsource and read it before using this file.
- * 
+ *
  * This Original Code and all software distributed under the License are
  * distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER
  * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
@@ -18,7 +18,7 @@
  * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT.  Please see the
  * License for the specific language governing rights and limitations
  * under the License.
- * 
+ *
  * @APPLE_LICENSE_HEADER_END@
  *
  * Updates:
@@ -106,12 +106,12 @@ TagPtr XMLGetProperty(TagPtr dict, const char * key)
 	{
 		TagPtr tag = 0;
 		TagPtr tagList = dict->tag;
-
+		
 		while (tagList)
 		{
 			tag = tagList;
 			tagList = tag->tagNext;
-
+			
 			if ((tag->type != kTagTypeKey) || (tag->string == 0))
 			{
 				continue;
@@ -122,7 +122,7 @@ TagPtr XMLGetProperty(TagPtr dict, const char * key)
 			}
 		}
 	}
-
+	
 	return 0;
 }
 
@@ -142,7 +142,7 @@ long XMLParseFile(char * buffer, TagPtr * dict)
 	while (TRUE)
 	{
 		length = XMLParseNextTag(buffer + pos, &tag);
-
+		
 		if (length == -1)
 		{
 			break;
@@ -159,7 +159,7 @@ long XMLParseFile(char * buffer, TagPtr * dict)
 		{
 			break;
 		}
-
+		
 		XMLFreeTag(tag);
 	}
 
@@ -169,7 +169,7 @@ long XMLParseFile(char * buffer, TagPtr * dict)
 	}
 
 	*dict = tag;
-
+	
 	return 0L;
 }
 #endif /* UNUSED */
@@ -182,11 +182,11 @@ long XMLParseNextTag(char * buffer, TagPtr * tag)
 	char * tagName;
 	long pos	= 0L;
 	long length	= GetNextTag(buffer, &tagName, 0);
-	
+
 	if (length != -1L)
 	{
 		pos = length;
-		
+
 		if (!strncmp(tagName, kXMLTagPList, 6))
 		{
 			length = 0L;
@@ -240,12 +240,11 @@ long XMLParseNextTag(char * buffer, TagPtr * tag)
 			*tag = 0;
 			length = 0L;
 		}
-		
+
 		if (length == -1)
 		{
 			pos = 0L;
 		}
-
 	}
 
 	return pos + length;
@@ -285,7 +284,7 @@ static long ParseTagList(char * buffer, TagPtr * tag, long type, long empty)
 		if (length == -1)
 		{
 			XMLFreeTag(tagList);
-
+			
 			return -1L;
 		}
 	}
@@ -378,7 +377,7 @@ static long ParseTagString(char * buffer, TagPtr * tag)
 				tmpTag->string	= string;
 				tmpTag->tag		= 0;
 				tmpTag->tagNext	= 0;
-				
+
 				*tag = tmpTag;
 			}
 			else
@@ -506,38 +505,38 @@ static long GetNextTag(char * buffer, char ** tag, long * start)
 	{
 		// Find the start of the tag.
 		long cnt = 0L;
-		
+
 		while ((buffer[cnt] != '\0') && (buffer[cnt] != '<'))
 		{
 			cnt++;
 		}
-		
+
 		if (buffer[cnt] != '\0')
 		{
 			// Find the end of the tag.
 			long cnt2 = cnt + 1L;
-			
+
 			while ((buffer[cnt2] != '\0') && (buffer[cnt2] != '>'))
 			{
 				cnt2++;
 			}
-			
+
 			if (buffer[cnt2] != '\0')
 			{
 				// Fix the tag data.
 				*tag = buffer + cnt + 1;
 				buffer[cnt2] = '\0';
-				
+
 				if (start)
 				{
 					*start = cnt;
 				}
-				
+
 				return cnt2 + 1;
 			}
 		}
 	}
-	
+
 	return -1L;
 }
 
@@ -658,7 +657,7 @@ static char * NewSymbol(char * string)
 			// Set the symbol's data.
 			symbol->refCount = 0;
 			strcpy(symbol->string, string);
-
+			
 			// Add the symbol to the list.
 			symbol->next = gSymbolsHead;
 			gSymbolsHead = symbol;
