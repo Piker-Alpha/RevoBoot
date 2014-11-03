58f20379d378242f6cb593f2e258b659da9f6499
Piker-Alpha <PikerAlpha@yahoo.com>
2014-11-03 16:50:04 +0100

    Updating script to v0.3
    – check arguments, use argv[1] as target table

--- i386/libsaio/acpi/Tools/extractACPITables.c
+++ i386/libsaio/acpi/Tools/extractACPITables.c
@@ -1,17 +1,21 @@
 /*
  * Name			: extractACPITables.c
- * Version		: 0.1
+ * Version		: 0.3
  * Type			: Command line tool
  * Copyright	: Pike R. Alpha (c) September 2014
  * Description	: ACPI table extractor.
  *
- * Usage		: ./extractACPITables	(writes table data to: /tmp/XXXX.aml)
+ * Usage		: ./extractACPITables			(writes all tables to: /tmp/XXXX.aml)
+ *				: ./extractACPITables DSDT		(writes DSDT.aml to: /tmp/DSDT.aml)
+ *				: ./extractACPITables SSDT		(writes all SSDT tables to: /tmp/SSDT.aml)
+ *				: ./extractACPITables SSDT-2	(writes SSDT-2.aml to: /tmp/SSDT-2.aml)
  *
  * Compile with	: cc -O2 extractACPITables.c -o extractACPITables -Wall -framework IOKit -framework CoreFoundation
  *
  * Updates:
  *			v0.1	initial version.
  *			v0.2	renamed from acpiTableExtract.c to extractAcpiTables.c
+ *			v0.3	check arguments, use argv[1] as target table.
  */
 
 #include <stdio.h>
@@ -24,12 +28,19 @@
 int main(int argc, char * argv[])
 {
 	char dirspec[32];
+	char tableName[8];
 	int filedesc, status = 0;
+	bool allTables = true;
 
 	io_service_t	service;
 	CFDictionaryRef	tableDictionary;
 
 	//==================================================================================
+
+	if (argc == 2)
+	{
+		allTables = false;
+	}
 
 	if ((service = IOServiceGetMatchingService(kIOMasterPortDefault, IOServiceMatching("AppleACPIPlatformExpert"))))
 	{
@@ -47,16 +58,21 @@ int main(int argc, char * argv[])
 				UInt8 * buffer = (UInt8 *) CFDataGetBytePtr(tableData[i]);
 				UInt32 numBytes = (UInt32) CFDataGetLength(tableData[i]);
 				
-				sprintf(dirspec, "/tmp/%s.aml", CFStringGetCStringPtr(tableNames[i], kCFStringEncodingMacRoman));
+				strcpy(tableName, CFStringGetCStringPtr(tableNames[i], kCFStringEncodingMacRoman));
 
-				if ((filedesc = open(dirspec, O_WRONLY|O_CREAT|O_TRUNC, 0644)) != -1)
+				if (allTables || (strncasecmp(argv[1], (char *)tableName, strlen(argv[1])) == 0))
 				{
-					write(filedesc, buffer, numBytes);
-					close(filedesc);
-				}
-				else
-				{
-					status = -3;
+					sprintf(dirspec, "/tmp/%s.aml", tableName);
+
+					if ((filedesc = open(dirspec, O_WRONLY|O_CREAT|O_TRUNC, 0644)) != -1)
+					{
+						write(filedesc, buffer, numBytes);
+						close(filedesc);
+					}
+					else
+					{
+						status = -3;
+					}
 				}
 			}
 
@@ -69,9 +85,10 @@ int main(int argc, char * argv[])
 
 		IOObjectRelease(service);
 
-		exit(status);	//  0 = success.
-						// -2 = tableDictionary is NULL.
-						// -3 = one or more writes failed.
+		//  0 = success.
+		// -2 = tableDictionary is NULL.
+		// -3 = one or more writes failed.
+		exit(status);
     }
 
 	exit(-1); // error (service is NULL).
